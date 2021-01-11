e81ce3d8d114f380e6ffc3e673047c3691e6d461
Maxim <vaslyaev.m.m@effective-group.ru>
2021-01-12 02:51:42 +0300

    Write to file

--- src/main/java/ru/digitalhabbits/homework2/FileProcessor.java
+++ src/main/java/ru/digitalhabbits/homework2/FileProcessor.java
@@ -1,11 +1,14 @@
 package ru.digitalhabbits.homework2;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 
 import javax.annotation.Nonnull;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.lang.Runtime.getRuntime;
 import static java.nio.charset.Charset.defaultCharset;
@@ -19,21 +22,61 @@ public class FileProcessor {
         checkFileExists(processingFileName);
 
         final File file = new File(processingFileName);
-        // TODO: NotImplemented: запускаем FileWriter в отдельном потоке
+        final File resultFile = new File(resultFileName);
+        if (!resultFile.exists()) {
+            try {
+                resultFile.createNewFile();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+
+        final AtomicInteger countRow = new AtomicInteger(1);
+        final Exchanger<Pair<String, Integer>> exchanger = new Exchanger<>();
+
+        final Thread writerThread = new Thread(new FileWriter(exchanger, resultFile, countRow));
+        final LineProcessor<Integer> processorCount = new LineCounterProcessor();
+        writerThread.setDaemon(true);
+        writerThread.start();
+        ExecutorService executorService = Executors.newFixedThreadPool(CHUNK_SIZE);
 
         try (final Scanner scanner = new Scanner(file, defaultCharset())) {
             while (scanner.hasNext()) {
-                // TODO: NotImplemented: вычитываем CHUNK_SIZE строк для параллельной обработки
+                int atomicCount = countRow.get();
+                String nextLine = scanner.nextLine();
+                executorService.execute(new Runnable() {
+                    int innerCountRow = atomicCount;
 
-                // TODO: NotImplemented: обрабатывать строку с помощью LineProcessor. Каждый поток обрабатывает свою строку.
-
-                // TODO: NotImplemented: добавить обработанные данные в результирующий файл
-            }
+                    @Override
+                    public void run() {
+                        Pair<String, Integer> process = processorCount.process(nextLine);
+                        while (innerCountRow != atomicCount) {
+                            try {
+                                Thread.sleep(100);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                                return;
+                            }
+                        }
+                        try {
+                            exchanger.exchange(process);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                countRow.getAndIncrement();
+              }
         } catch (IOException exception) {
-            logger.error("", exception);
+            logger.error("IOException", exception);
         }
 
-        // TODO: NotImplemented: остановить поток writerThread
+        try {
+            executorService.awaitTermination(2, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        executorService.shutdown();
 
         logger.info("Finish main thread {}", Thread.currentThread().getName());
     }
--- src/main/java/ru/digitalhabbits/homework2/FileWriter.java
+++ src/main/java/ru/digitalhabbits/homework2/FileWriter.java
@@ -1,6 +1,18 @@
 package ru.digitalhabbits.homework2;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.lang.Thread.currentThread;
 import static org.slf4j.LoggerFactory.getLogger;
@@ -9,9 +21,35 @@ public class FileWriter
         implements Runnable {
     private static final Logger logger = getLogger(FileWriter.class);
 
+    private final Exchanger<Pair<String, Integer>> exchanger;
+    private final File file;
+    private final AtomicInteger rawCounter;
+
+    public FileWriter(Exchanger<Pair<String, Integer>> exchanger, File file, AtomicInteger rawCounter) {
+        this.exchanger = exchanger;
+        this.file = file;
+        this.rawCounter = rawCounter;
+    }
+
     @Override
     public void run() {
         logger.info("Started writer thread {}", currentThread().getName());
+        Path path = Paths.get(file.toURI());
+        try {
+            BufferedWriter bufferedWriter = Files.newBufferedWriter(path);
+            while (!Thread.currentThread().isInterrupted()) {
+                Pair<String, Integer> exchange = exchanger.exchange(null);
+                String message = exchange.toString("%1$s %2$s");
+                bufferedWriter.write(message);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+                rawCounter.incrementAndGet();
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        } catch (InterruptedException e) {
+            logger.info("Thread is interrupted ");
+        }
 
         logger.info("Finish writer thread {}", currentThread().getName());
     }
